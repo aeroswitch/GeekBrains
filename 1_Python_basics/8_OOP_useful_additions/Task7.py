9005808bbc836f117c15ababaf34c1c6b0f66d15
aeroswitch <aeroswitch@gmail.com>
2020-07-09 19:48:11 +0300

    Added additional code implementation to 8_OOP_useful_addition_lessons

--- 1_Python_basics/8_OOP_useful_additions/Task7.py
+++ 1_Python_basics/8_OOP_useful_additions/Task7.py
@@ -23,3 +23,29 @@ print(f'Complex number 2:\n{complex_number_2.a} + {complex_number_2.b}i')
 print('*' * 20)
 print(f'Sum result:\n{complex_number_1 + complex_number_2}')
 print(f'Multiplying result:\n{complex_number_1 * complex_number_2}')
+
+#  -------------------------------------------------------- 7 ----------------------------------------------------------
+
+#
+#
+# class ComplexNumber:
+#     def __init__(self, real, imaginary):
+#         self.real = real
+#         self.img = imaginary
+#
+#     def __str__(self):
+#         return f'{self.real}+{self.img}i' if self.img > 0 else f'{self.real}{self.img}i'
+#
+#     def __add__(self, other):
+#         return ComplexNumber(self.real + other.real, self.img + other.img)
+#
+#     def __mul__(self, other):
+#         return ComplexNumber((self.real * other.real - self.img * other.img),
+#                              (self.img * other.real + self.real * other.img))
+#
+#
+# cn = ComplexNumber(1, -2)
+# cn1 = ComplexNumber(3, 4)
+# print(cn + cn1)
+# print(cn * cn1)
+# print(complex(1, -2) * complex(3, 4))  # calculation check
